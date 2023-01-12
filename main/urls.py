93801c9733aa11ea01bc1c7c3b8dd4173e4ae958
Cooper <dancersoftaranto@gmail.com>
2023-01-12 21:33:31 +0300

    image

--- main/urls.py
+++ main/urls.py
@@ -34,4 +34,5 @@ urlpatterns = [
     path('__debug__/', include('debug_toolbar.urls')),
 ]
 
-urlpatterns +=  static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
